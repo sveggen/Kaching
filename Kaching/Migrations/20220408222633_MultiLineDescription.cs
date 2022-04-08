3c687e90ea36b9786f58424c7815c099bf64a55e
sveggen <elfenbenet@protonmail.com>
2022-04-09 00:29:56 +0200

    Optional fields UI magic

---
+++ Kaching/Migrations/20220408222633_MultiLineDescription.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kaching.Migrations
+{
+    public partial class MultiLineDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
