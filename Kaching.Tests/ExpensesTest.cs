10b80fd98e3af9e598fb3b3f9269b157cbc03cba
sveggen <elfenbenet@protonmail.com>
2022-04-19 15:43:44 +0200

    Added some tests

--- Kaching.Tests/ExpensesTest.cs
+++ Kaching.Tests/ExpensesTest.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using Kaching.Controllers;
 using Kaching.Repositories;
 using Moq;
 using NUnit.Framework;
+using System.Threading.Tasks;
+using Kaching.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Kaching.Tests;
 
@@ -23,8 +27,52 @@ public class ExpensesTest
     }
 
     [Test]
-    public void Test1()
+    public async Task IndexGet_ReturnsListOfExpenses_WhenExpensesExists()
     {
-        Assert.Pass();
+        var randomMonthString = "March";
+        var randomMonthInt = 3;
+        var expenseList = new List<Expense>();
+        var personList = new List<Person>();
+
+        _mockExpenseRepository.Setup
+                (repo => repo.GetExpenses(randomMonthInt))
+            .ReturnsAsync(expenseList);
+
+        _mockPersonRepository.Setup
+                (repo => repo.GetAllPersons())
+            .Returns(personList);
+
+
+        var result = await _controller.Index(randomMonthString) as ViewResult;
+
+        //Assert.IsInstanceOf<List<Expense>>(result.Model);
+        Assert.IsInstanceOf<ViewResult>(result);
+    }
+
+
+    [Test]
+    public async Task IndexGet_ReturnsDecimal_WhenSumIsDisplayed()
+    {
+        var randomMonthString = "March";
+        var randomMonthInt = 3;
+        var expenseList = new List<Expense>();
+        var personList = new List<Person>();
+
+        _mockExpenseRepository.Setup
+                (repo => repo.GetExpenses(randomMonthInt))
+            .ReturnsAsync(expenseList);
+        _mockExpenseRepository.Setup
+                (repo => repo.GetExpenseSum(randomMonthInt))
+            .Returns(It.IsAny<decimal>());
+
+        _mockPersonRepository.Setup
+                (repo => repo.GetAllPersons())
+            .Returns(personList);
+
+
+        var viewResult = await _controller.Index(randomMonthString) as ViewResult;
+        var result = viewResult.ViewData["MonthExpenseSum"];
+
+        Assert.IsInstanceOf<decimal>(result);
     }
 }
