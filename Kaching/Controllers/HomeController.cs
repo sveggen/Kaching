442085768d23fe0f95e5dd3175d68f449ea59902
sveggen <elfenbenet@protonmail.com>
2022-05-01 17:54:21 +0200

    Fixed minor flaws

--- Kaching/Controllers/HomeController.cs
+++ Kaching/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using Kaching.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Kaching.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
