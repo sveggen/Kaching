5ada9de637cbf3fae869edc4b3fb02c2c3f75b15
sveggen <elfenbenet@protonmail.com>
2022-05-01 11:19:33 +0200

    Moved from SemanticUI to FomanticUI

--- Kaching/wwwroot/js/site.js
+++ Kaching/wwwroot/js/site.js
@@ -3,34 +3,6 @@
 
 // Write your JavaScript code.
 
-
-// Modal
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-})
-
-// Popover
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-})
-
-// Toast
-var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-var toastList = toastElList.map(function (toastEl) {
-    return new bootstrap.Toast(toastEl, option)
-})
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
-function disableButton(btn) {
-    document.getElementById("paymentReminder").disabled = true;
-}
-
-$('.ui.sidebar').sidebar({
-    context: $('.ui.pushable.segment'),
-    transition: 'overlay'
-}).sidebar('attach events', '#mobile_item');
+$('#monthdropdown')
+        .dropdown()
+    ;
